83cc2f5fc87d4ab5076c12c24fbaf37260db1aba
Orkhan Abdullayev <o.o.abdullayev@outlook.com>
2021-03-13 02:45:47 +0400

    Add ability to pass file path as an argument

--- HqPlusWebExtraction/Program.cs
+++ HqPlusWebExtraction/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 
 namespace HqPlusWebExtraction
@@ -9,9 +10,21 @@ namespace HqPlusWebExtraction
         static void Main(string[] args)
         {
             var extractor = new BookingDataExtractor();
+
             var currentDirectory = Directory.GetCurrentDirectory();
             var inputDataDirectory = Path.Combine(Directory.GetParent(currentDirectory).Parent.Parent.FullName, "input-data");
             var filePath = Path.Combine(inputDataDirectory, "Kempinski Hotel Bristol Berlin, Germany - Booking.com.html");
+
+            if (args.Any())
+            {
+                filePath = args[0];
+            }
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File not found :(");
+                return;
+            }
 
             var result = extractor.ExtractFromFile(filePath).Result;
 
